ebde3edeca7f09cee00c7dc10baa20f7303b3ee2
m3vance <m3vance@gmail.com>
2023-05-21 19:25:54 -0700

    assessment 5 complete

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,19 +10,60 @@ filter_letter_o = 'o'
 filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def letter_checker(array, letter)
+    array.select { |beverage| beverage.include?(letter) }
+end
+
+p letter_checker(beverages_array, filter_letter_o)
+p letter_checker(beverages_array, filter_letter_t)
+
+# output: ["coffee", "soda water"]
+#         ["tea", "water", "soda water"]
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], northeast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+def combine_and_sort(hash)
+    hash.values.flatten.sort
+end
+    
+p combine_and_sort(us_states)
+
+# output: ["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Rhode Island", "Washington"]
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+class Bike
+    attr_accessor :model, :wheels, :current_speed
 
+    def initialize(model, wheels = 2, current_speed = 0)
+        @model = model
+        @wheels = wheels
+        @current_speed = current_speed
+    end
+
+    def bike_info
+         "The #{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+    end
+
+    def pedal_faster(mushroom)
+        @current_speed += mushroom
+    end
+
+    def brake(banana)
+        @current_speed -= banana
+        if @current_speed < 0
+           @current_speed = 0
+        end
+    end
+end
+
+    # output: "The Trek bike has 2 wheels and is going 0 mph."
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -30,3 +71,12 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+trek = Bike.new('Trek')
+trek.pedal_faster(10)
+trek.pedal_faster(18)
+trek.brake(5)
+trek.brake(23)
+p trek.bike_info
+
+    # output: 10, 28, 23, 0
